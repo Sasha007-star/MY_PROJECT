db5f4e67fde245a9ade539032d0200230db89cf8
Sasha007-star <bondarukoleksandra74@gmail.com>
2025-03-01 11:32:24 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,123 +1,102 @@
 from pygame import *
-from random import randint
-import time as pytime
+from random import randint, choice
 
-
+# Ініціалізація
 font.init()
 font1 = font.Font(None, 80)
 font2 = font.Font(None, 36)
 
-img_back = 'background_fish.png'
+# Зображення
+img_back = 'background_fish.jpg'
 hero_img = 'vudka.png'
 enemy_img = 'fish.png'
 
+# Налаштування
 clock = time.Clock()
 FPS = 30
-
 score = 0
 goal = 50
 lost = 0
 max_lost = 5
 
-fish_caught = 0
-is_fishing = False
-time_to_wait = 3
-running = True
-
-class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        sprite.Sprite.__init__(self)
-        self.image = transform.scale(
-            image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
-
-        self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
-
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-
-class Player(GameSprite):
-    def update(self):
-        self.player = Player(self, 150, 250, 30, 40, 1)
-         
-         
-class Enemy(GameSprite):
-    enemy_x, enemy_y = 300,300
-    enemy_width, enemy_width = 100, 100
-    object_visible = True
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        if self.rect.y > win_height:
-            self.rect.x = randint(80, win_width - 80)
-            self.rect.y = 0
-            lost = lost + 1
-        
-             
 win_width = 600
 win_height = 700
 display.set_caption("Fishermen_simulator")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
 
+# Клас спрайта
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-vudka = Player(hero_img, 5, win_height - 100, win_width - 80, 100, 10)
+# Гравець (вудка)
+class Player(GameSprite):
+    def update(self):
+        self.rect.x, self.rect.y = mouse.get_pos()  # Вудка рухається за мишею
+        self.rect.y = win_height - 120  # Фіксуємо її по вертикалі
 
-monsters = sprite.Group()
-for i in range(1,6):
-    monster = Enemy(enemy_img, randint
-(80, win_width - 80), -40, 80, 50, randint(1,3))
-    monsters.add(monster)
+# Вороги (рибки)
+class Enemy(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__(player_image, player_x, player_y, size_x, size_y, player_speed)
+        self.direction = choice([-1, 1])  # Випадковий напрямок (ліворуч чи праворуч)
+    
+    def update(self):
+        self.rect.x += self.speed * self.direction  # Рух горизонтально
+          # Міняємо напрямок руху
 
+# Створення об'єктів
+vudka = Player(hero_img, 250, win_height - 120, 80, 100, 10)
+fishes = sprite.Group()
+for i in range(5):
+    fish = Enemy(enemy_img, randint(80, win_width - 80), randint(100, 500), 80, 50, randint(1, 3))
+    fishes.add(fish)
 
 finish = False
 run = True
 
-while run:  
-    if is_fishing:
-        time.sleep(time_to_wait)
-        fish_caught = randint.choice(True, False) 
-    if fish_caught:
-        window.blit(hero_img, (randint(100, 100, 100, 100)))
-        text1 = font2.render("Риба спіймана!" + str(score), 1, (0, 255,0))
-        window.blit(text, (10,50))
+# Головний цикл гри
+while run:
+    window.blit(background, (0, 0))
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == MOUSEBUTTONDOWN:
+            for fish in fishes:
+                if fish.rect.collidepoint(e.pos):  # Перевіряємо доторк миші
+                    fishes.remove(fish)  # Видаляємо рибу
+                    score += 1
+                    new_fish = Enemy(enemy_img, randint(80, win_width - 80), randint(100, 500), 80, 50, randint(1, 3))
+                    fishes.add(new_fish)
+    
     if not finish:
-        text = font2.render("Риба не клюнула!" + str(lost), 1, (0, 255,0))
-        window.blit(text, (10,50)) 
-
+        text = font2.render(f"Спіймано: {score} | Втрачено: {lost}", 1, (255, 255, 255))
+        window.blit(text, (10, 10))
+        
         vudka.update()
-        monsters.update()
-
+        fishes.update()
         vudka.reset()
-        monsters.draw(window)
+        fishes.draw(window)
 
-    collides = sprite.groupcollide(monsters, MOUSEBUTTONDOWN, True, True)
-    for collide in collides:
-            score = score + 1
-            monster = Enemy(
-                enemy_img, randint(80, win_width - 80), -40, 80, 50, randint(1, 5)
-            )
-            monsters.add(monster)
-        
-    if sprite.spritecollide(monsters, True):
-            lost -= 1
-            monster = Enemy(
-                enemy_img, randint(80,win_width - 80), -40, 80, 50, randint(1,5)
-            )
-            monsters.add(monster)
-        
-    if lost >= max_lost:
-            finish = True
-            mixer.music.stop()
-            window.blit(text, (200, 200))
-        
     if score >= goal:
-            finish = True
-            mixer.music.stop()
-            window.blit(text1, (200, 200))  
+        finish = True
+        window.blit(font1.render("Ти переміг!", True, (0, 255, 0)), (200, 300))
+    
+    display.update()
+    clock.tick(FPS)
 
 
-display.update() 
-clock.tick(FPS) 
+
+    
+
+
+    
